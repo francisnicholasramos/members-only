90ece3985e70c5f89a78557f6b6612e173fb7560
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-11-06 16:39:13 +0800

    Remove dist build

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import { router as members } from "./routes/membersRoute";
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "../src/views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "scripts")));
